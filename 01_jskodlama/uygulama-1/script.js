df1246d6833aec6558ef7ac4a1f2a4d6eb42f082
denizdogruel00 <denizyazilm@gmail.com>
2026-04-30 09:25:40 +0300

    uygulama yapıldı js degiskenler bitti

---
+++ 01_jskodlama/uygulama-1/script.js
@@ -0,0 +1,22 @@
+//ogrenci 1 bilgiler
+var ogrenci1isimsoyisim="ada bilgi";
+var ogrencidogumtarihi1=2012;
+var ogrenci1not1=70;
+var ogrenci1not2=70;
+var ogrenci1not3=80;
+var ortalama1=(ogrenci1not1+ogrenci1not2+ogrenci1not3)/3;
+var basariliMi1=(ortalama1>50);
+console.log(basariliMi1);
+//ogrenci 2 bilgileri
+var ogrenci2isimsoyisim="yiğit bilgi";
+var ogrencidogumtarihi2=2010;
+var ogrenci2not1=40;
+var ogrenci2not2=40;
+var ogrenci2not3=50;
+var ortalama2=(ogrenci2not1+ogrenci2not2+ogrenci2not3)/3;
+var basariliMi2=(ortalama2>50);
+console.log(basariliMi2);
+
+
+
+
