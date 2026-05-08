5404b6a8239047b7785f4fcf1ceb86c96297744a
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-08 15:51:09 +0300

    if-else uygulaamasi ve string methodlari uygulamsı eklendi

---
+++ Uygulamalar/String-methodlari-uygluamalar/script.js
@@ -0,0 +1,16 @@
+let url="https://www.sadikturan.com/";
+let kursadi="Komple Uygulamalı Web Geliştirme Kursu";
+let sonuc;
+//1) sonuc=url.length;
+// console.log(sonuc);
+//2) sonuc=kursadi.split(" ").length;
+// console.log(sonuc)
+//3) sonuc=url.startsWith("https");
+// console.log(sonuc);
+//4) sonuc=kursadi.includes("Eğitimi");
+// console.log(sonuc);
+//5) kursadi=kursadi.replaceAll(" ","-");
+// kursadi=kursadi.toLowerCase();
+// sonuc=url+kursadi;
+// console.log(sonuc);
+
